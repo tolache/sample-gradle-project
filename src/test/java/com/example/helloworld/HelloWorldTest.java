6454b48a53cc6a6f3ea2d3857e237de84bf1df8d
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2021-12-20 19:27:27 +0300

    add gradle sleep task

--- src/test/java/com/example/helloworld/HelloWorldTest.java
+++ src/test/java/com/example/helloworld/HelloWorldTest.java
@@ -3,9 +3,6 @@ package com.example.helloworld;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
 public class HelloWorldTest {
     @Test
     public void printShouldSuccess() throws Exception {
